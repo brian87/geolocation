ddc89f5f5091eb090a24d9ce20cf095c30484291
Braiyan <brian.aguilar.b@gmail.com>
2020-07-08 14:41:43 -0500

    fixing Geolocation Service

--- src/main/java/com/apress/ws/endpoint/GeoLocationEndpoint.java
+++ src/main/java/com/apress/ws/endpoint/GeoLocationEndpoint.java
@@ -1,10 +1,14 @@
 package com.apress.ws.endpoint;
 
-import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.apress.dto.LocationDTO;
+import com.apress.service.LocationService;
+import com.apress.service.mappers.LocationMapper;
 
 import garage.services.geolocation.types.GetLocation;
 import garage.services.geolocation.types.GetLocationResponse;
@@ -12,14 +16,18 @@ import garage.services.geolocation.types.GetLocationResponse;
 @Endpoint
 public class GeoLocationEndpoint {
 
+	@Autowired
+	private LocationService locationService;
+	@Autowired
+	private LocationMapper locationMapper;
+
 	@PayloadRoot(namespace = "urn:garage:services:geolocation:types", localPart = "GetLocation")
 	@ResponsePayload
 	public GetLocationResponse getLocation(@RequestPayload GetLocation getLocationRequest) {
-		System.out.println(getLocationRequest.getIp());
-		GetLocationResponse response = new GetLocationResponse();
-		String url = "http://ip-api.com/json/";
-		RestTemplate restTemplate = new RestTemplate();
-		response = restTemplate.getForObject(url + getLocationRequest.getIp(), GetLocationResponse.class);
+
+		LocationDTO locationDTO = locationService.getLocationByIp(getLocationRequest.getIp());
+		GetLocationResponse response = locationMapper.toGetLocationResponse(locationDTO);
+
 		return response;
 
 	}
