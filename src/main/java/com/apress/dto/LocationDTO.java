369720a1f4d595e1bf8f5662756e9d6fd06fba43
Braiyan <brian.aguilar.b@gmail.com>
2020-07-08 14:42:02 -0500

    adding LocationDTO

---
+++ src/main/java/com/apress/dto/LocationDTO.java
@@ -0,0 +1,35 @@
+package com.apress.dto;
+
+public class LocationDTO {
+
+	protected String status;
+
+	protected String country;
+
+	protected String city;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
