6a7b9ef1424f177f10edc7808a2379f6eb5b3a47
Braiyan <brian.aguilar.b@gmail.com>
2020-07-08 22:41:54 -0500

    borrando la wsdl

--- src/main/java/com/apress/GeolocationApplication.java
+++ src/main/java/com/apress/GeolocationApplication.java
@@ -2,12 +2,23 @@ package com.apress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.xml.transform.StringResult;
+
+import garage.services.geolocation.types.GetLocationRequest;
 
 @SpringBootApplication
 public class GeolocationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeolocationApplication.class, args);
+		GetLocationRequest request = new GetLocationRequest();
+		request.setIp("190.234.150.194");
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setPackagesToScan("garage.services.geolocation.types");
+		StringResult res = new StringResult();
+		marshaller.marshal(request, res);
+		System.out.println(res);
 	}
 
 }
