2b13c14ca41c4d498f1020d9dbcfdffda3076a51
Braiyan <brian.aguilar.b@gmail.com>
2020-07-10 08:03:36 -0500

    adding getHosAddress

--- src/main/java/com/apress/service/LocationService.java
+++ src/main/java/com/apress/service/LocationService.java
@@ -13,11 +13,28 @@ public class LocationService {
 
 	public LocationDTO getLocationByIp(String ip) {
 		RestTemplate restTemplate = new RestTemplate();
+		ip = getHostAddress(ip);
 		if (isPrivateIPAddress(ip)) {
 			ip = restTemplate.getForObject("https://api.ipify.org/", String.class);
 		}
 
 		return restTemplate.getForObject("http://ip-api.com/json/" + ip, LocationDTO.class);
+	}
+
+	private String getHostAddress(String ip) {
+		InetAddress inetAddress = null;
+		if (ip.equalsIgnoreCase("0:0:0:0:0:0:0:1")) {
+
+			try {
+				inetAddress = InetAddress.getLocalHost();
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String ipAddress = inetAddress.getHostAddress();
+			ip = ipAddress;
+		}
+		return ip;
 	}
 
 	private static boolean isPrivateIPAddress(String ipAddress) {
@@ -33,4 +50,5 @@ public class LocationService {
 
 		return ia.isSiteLocalAddress();
 	}
+
 }
