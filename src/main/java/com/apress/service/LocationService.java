8464937b54db55c73fec262944f175de13f21b86
Braiyan <brian.aguilar.b@gmail.com>
2020-07-10 11:42:16 -0500

    fixing

--- src/main/java/com/apress/service/LocationService.java
+++ src/main/java/com/apress/service/LocationService.java
@@ -13,28 +13,24 @@ public class LocationService {
 
 	public LocationDTO getLocationByIp(String ip) {
 		RestTemplate restTemplate = new RestTemplate();
-//		ip = getHostAddress(ip);
+		// if ip is "0:0:0:0:0:0:0:1" then find the hostAddress
+		if (ip.equalsIgnoreCase("0:0:0:0:0:0:0:1")) {
+			ip = getIpLocalHost(ip);
+		}
 		if (isPrivateIPAddress(ip)) {
 			ip = restTemplate.getForObject("https://api.ipify.org/", String.class);
 		}
-
 		return restTemplate.getForObject("http://ip-api.com/json/" + ip, LocationDTO.class);
 	}
 
-	private String getHostAddress(String ip) {
+	private String getIpLocalHost(String ip) {
 		InetAddress inetAddress = null;
-		if (ip.equalsIgnoreCase("0:0:0:0:0:0:0:1")) {
-
-			try {
-				inetAddress = InetAddress.getLocalHost();
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			String ipAddress = inetAddress.getHostAddress();
-			ip = ipAddress;
+		try {
+			inetAddress = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
 		}
-		return ip;
+		return inetAddress.getHostAddress();
 	}
 
 	private static boolean isPrivateIPAddress(String ipAddress) {
