1336877081ba36c5f864300cd142c842e4c9e7cc
Braiyan <brian.aguilar.b@gmail.com>
2020-07-11 21:54:34 -0500

    Fixing IsPrivateIp

--- src/main/java/com/apress/service/LocationService.java
+++ src/main/java/com/apress/service/LocationService.java
@@ -1,5 +1,8 @@
 package com.apress.service;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,11 +12,11 @@ import com.apress.dto.LocationDTO;
 public class LocationService {
 
 	public LocationDTO getLocationByIp(String ip) {
-		if(isPrivateIp(ip)) {
+		if (isPrivateIp(ip) || ip.equalsIgnoreCase("0:0:0:0:0:0:0:1")) {
 			ip = getPublicIp();
 		}
 		RestTemplate restTemplate = new RestTemplate();
-		LocationDTO locationDTO  = restTemplate.getForObject("http://ip-api.com/json/" + ip, LocationDTO.class);
+		LocationDTO locationDTO = restTemplate.getForObject("http://ip-api.com/json/" + ip, LocationDTO.class);
 		return locationDTO;
 	}
 
@@ -23,7 +26,15 @@ public class LocationService {
 	}
 
 	private boolean isPrivateIp(String ip) {
-		return ip.equalsIgnoreCase("0:0:0:0:0:0:0:1");
+		InetAddress ia = null;
+		try {
+			InetAddress ad = InetAddress.getByName(ip);
+			byte[] ipAddress = ad.getAddress();
+			ia = InetAddress.getByAddress(ipAddress);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		return ia.isSiteLocalAddress();
 	}
-	
+
 }
